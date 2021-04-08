13e81aebfba238fec64394c1037b04b3e03bb9b6
Kency <Kencylantigua027@gmail.com>
2021-04-07 20:16:55 -0400

    video2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 
+import { Counter } from './components/counter';
+import { Usuario } from './components/Usuario';
 
 function App() {
   return (
     <>
-    <h1>React + Typescript</h1>  
+    <h1>React + Typescript</h1> 
+    <hr/>
+
+<Counter />
+<Usuario />
     </>
   );
 }
